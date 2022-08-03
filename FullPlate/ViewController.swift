0b03d4f5a785376e4db73b3edd2411be8d204acb
Kyra Ezikeuzor <kyraezikeuzor@gmail.com>
2022-08-03 09:12:15 -0500

    Initial Commit

---
+++ FullPlate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FullPlate
+//
+//  Created by scholar on 8/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
