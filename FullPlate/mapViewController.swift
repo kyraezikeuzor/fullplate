490b7269be25ca013322fb40caa3fab4ed2c5c6f
meganxkwok <110234107+meganxkwok@users.noreply.github.com>
2022-08-05 19:51:50 +0800

    map

--- FullPlate/mapViewController.swift
+++ FullPlate/mapViewController.swift
@@ -8,27 +8,35 @@
 import UIKit
 import MapKit
 
-class mapViewController: UIViewController {
+class mapViewController: UIViewController,MKMapViewDelegate {
 
     
     @IBOutlet weak var mapView: MKMapView!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+   
         let pin1 = MKPointAnnotation()
+        pin1.title = "Noglu"
+        pin1.subtitle = "$$; 1266 Madison Ave, New York, NY 10128"
         pin1.coordinate = CLLocationCoordinate2D(latitude: 40.7836381, longitude: -73.956963)
-        mapView.addAnnotation(pin1)
+                mapView.addAnnotation(pin1)
+
+                
+                let pin2 = MKPointAnnotation()
+        pin2.title = "Modern Bread and Bagel"
+        pin2.subtitle = "$, 472 Columbus Ave, New York, NY 10024"
+                pin2.coordinate = CLLocationCoordinate2D(latitude: 40.7844219, longitude: -73.9761515)
+                mapView.addAnnotation(pin2)
+                
+                let pin3 = MKPointAnnotation()
+        pin3.title = "Senza Gluten"
+        pin3.subtitle = "$$$, 206 Sullivan St, New York, New York"
+                pin3.coordinate = CLLocationCoordinate2D(latitude: 40.7291176, longitude: -74.0025261)
+                mapView.addAnnotation(pin3)
         
-        let pin2 = MKPointAnnotation()
-        pin2.coordinate = CLLocationCoordinate2D(latitude: 40.7844219, longitude: -73.9761515)
-        mapView.addAnnotation(pin2)
-        
-        let pin3 = MKPointAnnotation()
-        pin3.coordinate = CLLocationCoordinate2D(latitude: 40.7291176, longitude: -74.0025261)
-        mapView.addAnnotation(pin3)
     }
+
     
 
     /*
